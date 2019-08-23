b82e86f7e12e47b15f3f3d3cb4b97fb802888679
Dimas Arya Murdiyan <dimasaryamurdiyan123@gmail.com>
2019-08-23 16:48:54 +0700

    merging master

--- app/Model/Complaint.php
+++ app/Model/Complaint.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Complaint extends Model
 {
-    //
+    protected $table='complaints';
 }
--- app/Model/ProductEngine.php
+++ app/Model/ProductEngine.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Engine extends Model
+class ProductEngine extends Model
 {
     //
 }
